59803f9368a9603dfe4aa54c7d37f30d037ff53e
Noah Laux <noahlaux@gmail.com>
2014-01-15 16:30:15 +0100

    Removed obsolete code

--- jquery-prestige.js
+++ jquery-prestige.js
@@ -10,10 +10,6 @@
                 css = options.css || {},
                 width;
 
-            if (options.multiple) {
-                input.attr('multiple', true);
-            }
-
             div.css($.extend({
                 'position': 'absolute',
                 'top': '0px',
